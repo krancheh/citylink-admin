cd7b3074f06cda6b5152a72586137b231ab963be
Kirill <krancheh@gmail.com>
2024-04-23 12:04:59 +0300

    fix adaptive styles

--- src/components/CustomNoRowsMessage.tsx
+++ src/components/CustomNoRowsMessage.tsx
@@ -3,7 +3,9 @@ import { Box, Typography } from '@mui/material'
 const CustomNoRowsMessage = () => {
     return (
         <Box height="100%" display="flex" flexDirection="column" justifyContent="center" alignItems="center" bgcolor="#fdfdfd">
-            <img style={{ opacity: 0.8 }} src={require('../assets/icons/no-rows.svg').default} alt="No data icon" />
+            <Box height={{ sm: "auto", xs: "100px" }}>
+                <img style={{ opacity: 0.8, height: "100%" }} src={require('../assets/icons/no-rows.svg').default} alt="No data icon" />
+            </Box>
             <Typography>Записей нет</Typography>
         </Box>
     )
