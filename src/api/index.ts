bc194c315856e14503f6a0e85a1421229148828a
Kirill <krancheh@gmail.com>
2024-04-24 22:04:26 +0300

    add patch http method

--- src/api/index.ts
+++ src/api/index.ts
@@ -33,6 +33,7 @@ export const createApiFromPath = (basePath: string) => ({
     post: (path: string, ...args: any[]) => $api.post(`${basePath}/${path}`, ...args),
     put: (path: string, ...args: any[]) => $api.put(`${basePath}/${path}`, ...args),
     delete: (path: string, ...args: any[]) => $api.delete(`${basePath}/${path}`, ...args),
+    patch: (path: string, ...args: any[]) => $api.patch(`${basePath}/${path}`, ...args),
 })
 
 export default $api;
